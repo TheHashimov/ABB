00d45dc36b4d6553908df15d44a5bdd87aabee9d
TheHashimov <Thehashimov@gmail.com>
2025-10-31 17:46:01 +0400

    Task-6

---
+++ Lesson-4/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        Book b1 = new Book("Clean Code", "Robert C. Martin", "1234567890");
+
+        b1.printInfo();
+        b1.borrowBook();
+        b1.returnBook();
+        Book.libraryRules();
+        b1.bookType();
+    }
+}
